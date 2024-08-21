cc26a12e1b4100d0d805cca117c715b2bd780614
Lidiia90 <liduska200412@gmail.com>
2024-08-21 19:16:43 +0300

    Registration tests

--- src/main/java/screens/SplashScreen.java
+++ src/main/java/screens/SplashScreen.java
@@ -1,6 +1,7 @@
 package screens;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -9,10 +10,15 @@ public class SplashScreen extends BaseScreen{
         super(driver);
     }
     @FindBy(xpath = "//*[@resource-id='com.telran.ilcarro:id/versionText']")
-AndroidElement versionApp;
+    MobileElement versionApp;
 
 
     public boolean validateVersion() {
-        return textInElementPresent(versionApp, "Version 1.0.0", 3);
+        return textInElementPresent((AndroidElement) versionApp, "Version 1.0.0", 3);
+    }
+
+    public SearchScreen goToSearchScreen() {
+        pause(5);
+        return new SearchScreen(driver);
     }
 }
